09bc37922dac972c607a7e6d6e68e3614a5fd4e1
32d <105021055@live.asia.edu.tw>
2016-09-19 15:47:32 +0800

    ex課堂練習繳交

--- week01_20160919/src/ex/ex01_105021055.java
+++ week01_20160919/src/ex/ex01_105021055.java
@@ -1,15 +1,20 @@
 package ex;
+import java.util.Scanner;
 /*
  * Topic: 請寫一個程式讓使用者輸入「華氏」溫度，輸出「攝氏」溫度 (華氏 = 攝氏*(9/5)+32)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021055 王浚維
  */
 
-public class ex01_1050210XX {
+public class ex01_105021055 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scn =  new Scanner (System.in);
+		int F = 99 ;
+		int C = ( F - 32 )* 5/9 ;
+		System.out.println ("攝氏:"+ C  );
 	}
 
 }
--- week01_20160919/src/ex/ex02_105021055.java
+++ week01_20160919/src/ex/ex02_105021055.java
@@ -1,15 +1,20 @@
 package ex;
+import java.util.Scanner ;
 /*
  * Topic: 請寫一個程式讓使用者輸入體重單位 "公斤"，轉換成 "磅" (1磅 = 0.45359 公斤)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021055 王浚維
  */
 
-public class ex02_1050210XX {
+public class ex02_105021055 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scr = new Scanner (System.in);
+		int kg = 88 ; 
+		double  gg =  (kg /0.45359) ;
+		System.out.println (gg + "磅");
 	}
 
 }
