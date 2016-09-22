9a888e1a7ec22e5ca86ddffd93ea47f89b58e413
32d <105021055@live.asia.edu.tw>
2016-09-22 19:31:07 +0800

    105021055

---
+++ week01_20160919/src/hw/hw02_105021055.java
@@ -0,0 +1,15 @@
+package hw;
+import java.util.Random;
+import java.util.Scanner;
+public class hw02_105021055 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random ran = new Random();
+		int a = 6;
+		
+		System.out.println (ran.nextInt(a)+1); 
+	}
+
+}
