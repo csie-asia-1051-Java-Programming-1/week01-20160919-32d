e35865cc62a8ac803ef47d4ca59bd897e024b470
32d <105021055@live.asia.edu.tw>
2016-09-25 13:17:42 +0800

    105021055

---
+++ week01_20160919/src/hw/hw04_105021055.java
@@ -0,0 +1,13 @@
+package hw;
+import java.util.Scanner;
+public class hw04_105021055 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 int a = 9;
+     double b = 3.0;
+     System.out.println (Math.pow(a,(double)1/b));
+	}
+
+}
