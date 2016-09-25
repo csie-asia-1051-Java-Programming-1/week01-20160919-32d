94e69d8972ac02703339cc47e9076494bee7be36
32d <105021055@live.asia.edu.tw>
2016-09-25 15:53:51 +0800

    105021055

---
+++ week01_20160919/src/hw/hw01_105021055.java
@@ -0,0 +1,12 @@
+package hw;
+import java.util.Scanner;
+public class hw01_105021055 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int n = 9;
+      System.out.println(Math.ceil(Math.log(n)/Math.log(2)));
+	
+	}
+}
